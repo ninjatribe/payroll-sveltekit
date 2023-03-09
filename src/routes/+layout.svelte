ce891e83fb3c69e91404a99d622e57929d6e0871
NinjaOJTAloveraJr <126435661+NinjaOJTAloveraJr@users.noreply.github.com>
2023-03-09 17:33:59 +0800

    Conflict fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script>
-    	import '../app.css';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+	import '../app.css';
+
+	$: {
+		if (!$page.data.user) {
+			goto('/auth/login');
+		}
+	}
 </script>
+
 <svelte:head>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+	/>
 </svelte:head>
-<main id="content" class="min-h-[100vh] w-full max-w-screen">
-    <slot />
+<main id="content" class="min-h-[100vh] w-screen max-w-screen">
+	<slot />
 </main>
