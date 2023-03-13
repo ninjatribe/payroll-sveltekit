3e3c7aefb9d384bff204a2e6ba0fd252a13db87b
NinjaOJTAloveraJr <126435661+NinjaOJTAloveraJr@users.noreply.github.com>
2023-03-13 16:47:07 +0800

    Added User page

--- src/routes/api/admin/user/+server.ts
+++ src/routes/api/admin/user/+server.ts
@@ -3,25 +3,15 @@ import clientPromise from '$lib/server/mongo';
 /** @type {import('./$types').RequestHandler} */
 export async function GET({request, locals}: any) {
     const db = await clientPromise();
-    const Student = db.collection('students');
     const user = db.collection('users');
 
-    const response = await Student.find({}).toArray();
-    const uresponse = await user.find({}).toArray();
+    const response = await user.find({}).toArray();
 
     if(response) {
         return new Response(
             JSON.stringify({
                 status: 'Success',
                 response
-            })
-        )
-    }
-    if(uresponse) {
-        return new uresponse(
-            JSON.stringify({
-                status: 'Success',
-                uresponse
             })
         )
     }
