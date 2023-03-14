9b088eae70571bdabab3209c5f1bb8ab6e7c1ca6
NinjaOJTAloveraJr <126435661+NinjaOJTAloveraJr@users.noreply.github.com>
2023-03-14 12:49:16 +0800

    forgot to save server.ts

--- src/routes/api/admin/student/+server.ts
+++ src/routes/api/admin/student/+server.ts
@@ -4,24 +4,14 @@ import clientPromise from '$lib/server/mongo';
 export async function GET({request, locals}: any) {
     const db = await clientPromise();
     const Student = db.collection('students');
-    const user = db.collection('users');
 
     const response = await Student.find({}).toArray();
-    const uresponse = await user.find({}).toArray();
 
     if(response) {
         return new Response(
             JSON.stringify({
                 status: 'Success',
                 response
-            })
-        )
-    }
-    if(uresponse) {
-        return new uresponse(
-            JSON.stringify({
-                status: 'Success',
-                uresponse
             })
         )
     }
