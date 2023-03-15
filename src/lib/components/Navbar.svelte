0d5e99366141a7e5aafea4fa50771bf11f88bc71
Jed Adryon Fernandez <104202560+Adryonjed@users.noreply.github.com>
2023-03-15 19:37:23 +0800

    fernandez

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -31,7 +31,7 @@
 <nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
   <div class="px-3 py-2 lg:px-5 lg:pl-3">
     <div class="flex items-center justify-between">
-        <a href="/" class="flex ml-2 md:mr-24">
+        <a href="/" class="flex ml-2 md:mr-24" style="text-decoration-line: none;">
           <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
           <span class="text-xl font-semibold dark:text-white text-gray-900" >Payroll System</span>
         </a>
